2334078b0272d1037bc667136cfbcffe4660edac
Saidat-Ajoks <olaidesaheedah2015@gmail.com>
2026-07-30 01:03:20 -0700

    fix: reload page before valid login

--- cypress/e2e/saucedemos.cy.js
+++ cypress/e2e/saucedemos.cy.js
@@ -1,17 +1,22 @@
 describe('SauceDemo Automation', () => {
   it('completes the entire user journey', () => {
+    // 1. Invalid Login
     cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('wronguser')
     cy.get('[data-test="password"]').type('wrongpassword')
     cy.get('[data-test="login-button"]').click()
     cy.get('[data-test="error"]').should('be.visible')
 
+    // 2. Clear page & Valid Login
+    cy.visit('https://www.saucedemo.com/')
     cy.login()
     cy.url().should('include', 'inventory')
 
+    // 3. Sort Filter
     cy.get('[data-test="product-sort-container"]').select('az').should('have.value', 'az')
     cy.get('[data-test="product-sort-container"]').select('lohi').should('have.value', 'lohi')
 
+    // 4. Cart Operations
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
     cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
     cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
@@ -22,6 +27,7 @@ describe('SauceDemo Automation', () => {
     cy.get('[data-test="remove-sauce-labs-bike-light"]').click()
     cy.get('[data-test="shopping-cart-badge"]').should('have.text', '2')
 
+    // 5. Checkout
     cy.get('[data-test="shopping-cart-link"]').click()
     cy.get('[data-test="checkout"]').click()
     cy.get('[data-test="firstName"]').type('Ajoke')
